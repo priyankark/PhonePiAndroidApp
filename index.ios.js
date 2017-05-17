36aad1e3985daccb272bbd3ca9e20ef462160da5
Priyankar <priyankar.kumar98@gmail.com>
2017-05-17 11:55:34 +0530

    Added License

--- index.ios.js
+++ index.ios.js
@@ -11,43 +11,13 @@ import {
   Text,
   View
 } from 'react-native';
+import App from './src/App';
 
-export default class PhonePi extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+const PhonePi=()=>{
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+return(<App/>);
+
+};
+
 
 AppRegistry.registerComponent('PhonePi', () => PhonePi);
